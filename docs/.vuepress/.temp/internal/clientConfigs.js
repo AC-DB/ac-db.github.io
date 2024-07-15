231ec198d46bf9e9f7a01b3add606ecf7b198030
L-Win <a-l_l-a@outlook.com>
2024-07-15 22:39:09 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   docs/.vuepress/.cache/deps/@vue_devtools-api.js
    	new file:   docs/.vuepress/.cache/deps/@vue_devtools-api.js.map
    	new file:   docs/.vuepress/.cache/deps/@vuepress_shared.js
    	new file:   docs/.vuepress/.cache/deps/@vuepress_shared.js.map
    	new file:   docs/.vuepress/.cache/deps/_metadata.json
    	new file:   docs/.vuepress/.cache/deps/chunk-D2YVLGJ5.js
    	new file:   docs/.vuepress/.cache/deps/chunk-D2YVLGJ5.js.map
    	new file:   docs/.vuepress/.cache/deps/chunk-ZRZ4CPYY.js
    	new file:   docs/.vuepress/.cache/deps/chunk-ZRZ4CPYY.js.map
    	new file:   docs/.vuepress/.cache/deps/package.json
    	new file:   docs/.vuepress/.cache/deps/vue-router.js
    	new file:   docs/.vuepress/.cache/deps/vue-router.js.map
    	new file:   docs/.vuepress/.cache/deps/vue.js
    	new file:   docs/.vuepress/.cache/deps/vue.js.map
    	new file:   docs/.vuepress/.temp/internal/clientConfigs.js
    	new file:   docs/.vuepress/.temp/internal/routes.js
    	new file:   docs/.vuepress/.temp/internal/siteData.js
    	new file:   docs/.vuepress/.temp/internal/themeData.js
    	new file:   docs/.vuepress/.temp/pages/404.html.js
    	new file:   docs/.vuepress/.temp/pages/404.html.vue
    	new file:   docs/.vuepress/.temp/pages/index.html.js
    	new file:   docs/.vuepress/.temp/pages/index.html.vue
    	new file:   docs/.vuepress/.temp/styles/index.scss
    	new file:   docs/.vuepress/.temp/styles/palette.scss
    	new file:   docs/.vuepress/.temp/vite-root/index.html
    	new file:   docs/.vuepress/config.js
    	new file:   docs/README.md
    	modified:   package.json

---
+++ docs/.vuepress/.temp/internal/clientConfigs.js
@@ -0,0 +1,17 @@
+import * as clientConfig0 from 'C:/Users/A-bcd/Desktop/Program/GitHub/ac-db.github.io/node_modules/.pnpm/@vuepress+plugin-active-header-links@2.0.0-rc.38_vuepress@2.0.0-rc.14_@vuepress+bundler-vite@_pftbbtwfu2ghuem6nujsfeafny/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
+import * as clientConfig1 from 'C:/Users/A-bcd/Desktop/Program/GitHub/ac-db.github.io/node_modules/.pnpm/@vuepress+plugin-back-to-top@2.0.0-rc.38_vuepress@2.0.0-rc.14_@vuepress+bundler-vite@2.0.0-rc_hasaeg2sdv7firckrkrf6zbhdm/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
+import * as clientConfig2 from 'C:/Users/A-bcd/Desktop/Program/GitHub/ac-db.github.io/node_modules/.pnpm/@vuepress+plugin-copy-code@2.0.0-rc.38_vuepress@2.0.0-rc.14_@vuepress+bundler-vite@2.0.0-rc.1_e74kiq65qp6hgbrl7qgrigbmta/node_modules/@vuepress/plugin-copy-code/lib/client/config.js'
+import * as clientConfig3 from 'C:/Users/A-bcd/Desktop/Program/GitHub/ac-db.github.io/node_modules/.pnpm/@vuepress+plugin-medium-zoom@2.0.0-rc.38_vuepress@2.0.0-rc.14_@vuepress+bundler-vite@2.0.0-rc_dg2e2jpdeigxi3qkdnx7zuwi3u/node_modules/@vuepress/plugin-medium-zoom/lib/client/config.js'
+import * as clientConfig4 from 'C:/Users/A-bcd/Desktop/Program/GitHub/ac-db.github.io/node_modules/.pnpm/@vuepress+plugin-nprogress@2.0.0-rc.38_vuepress@2.0.0-rc.14_@vuepress+bundler-vite@2.0.0-rc.1_4beodq3shjx5pjrk2nugi2isxe/node_modules/@vuepress/plugin-nprogress/lib/client/config.js'
+import * as clientConfig5 from 'C:/Users/A-bcd/Desktop/Program/GitHub/ac-db.github.io/node_modules/.pnpm/@vuepress+plugin-theme-data@2.0.0-rc.38_vuepress@2.0.0-rc.14_@vuepress+bundler-vite@2.0.0-rc._rvfcxgpluz77v2y2xzbjl5vmku/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
+import * as clientConfig6 from 'C:/Users/A-bcd/Desktop/Program/GitHub/ac-db.github.io/node_modules/.pnpm/@vuepress+theme-default@2.0.0-rc.38_vuepress@2.0.0-rc.14_@vuepress+bundler-vite@2.0.0-rc.14_@_a5gljb6dcg5tocyoxcy465ysp4/node_modules/@vuepress/theme-default/lib/client/config.js'
+
+export const clientConfigs = [
+  clientConfig0,
+  clientConfig1,
+  clientConfig2,
+  clientConfig3,
+  clientConfig4,
+  clientConfig5,
+  clientConfig6,
+].map((m) => m.default).filter(Boolean)
