ad1ef2757902d08336f710ffe20a1c0974a87afd
L-Win <a-l_l-a@outlook.com>
2024-07-15 22:58:56 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   docs/.vuepress/.temp/internal/routes.js
    	modified:   docs/.vuepress/.temp/internal/siteData.js
    	modified:   docs/.vuepress/.temp/internal/themeData.js
    	modified:   docs/.vuepress/.temp/pages/404.html.js
    	modified:   docs/.vuepress/.temp/pages/index.html.js
    	new file:   docs/.vuepress/dist/404.html
    	new file:   docs/.vuepress/dist/assets/404.html-BkZmHLI9.js
    	new file:   docs/.vuepress/dist/assets/app-CXfKKde3.js
    	new file:   docs/.vuepress/dist/assets/index.html-CPNLjdrN.js
    	new file:   docs/.vuepress/dist/assets/style-C7hg6PmC.css
    	new file:   docs/.vuepress/dist/index.html

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,16 +1,3 @@
 import comp from "C:/Users/A-bcd/Desktop/Program/GitHub/ac-db.github.io/docs/.vuepress/.temp/pages/index.html.vue"
-const data = JSON.parse("{\"path\":\"/\",\"title\":\"Hello VuePre\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1721054349000,\"contributors\":[{\"name\":\"L-Win\",\"email\":\"a-l_l-a@outlook.com\",\"commits\":1}]},\"filePathRelative\":\"README.md\"}")
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"Hello VuePre\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1721054452000,\"contributors\":[{\"name\":\"L-Win\",\"email\":\"a-l_l-a@outlook.com\",\"commits\":2}]},\"filePathRelative\":\"README.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
